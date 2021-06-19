403ea66a9f6dafaa98ec584ea643e48d1c7b1624
Sungkeun Kim <ksungkeun84@tamu.edu>
2021-06-19 02:52:32 -0400

    .

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# install nvim
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+mkdir -p $HOME/.bin/nvim
+mv nvim.appimage $HOME/.bin/nvim
+echo "alias vi="nvim.appimage"" >> $HOME/.bashrc
+
+# copy init.vim
 mkdir -p ~/.config/nvim
 cp init.vim $HOME/.config/nvim/
 
-mkdir -p $HOME/.bin
-cp nvim.appimage $HOME/.bin
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "export PATH=$HOME/.bin:$PATH" > $HOME/.bashrc
+# install node.js (including npm and node)
+wget https://nodejs.org/dist/v14.17.1/node-v14.17.1-linux-x64.tar.xz
+tar xvf node-v14.17.1-linux-x64.tar.xz
+mv node-v14.17.1-linux-x64 $HOME/.bin/
+rm node-v14.17.1-linux-x64.tar.xz
+
+echo "export PATH=$HOME/.bin/node-v14.17.1-linux-x64/bin:$HOME/.bin/nvm:$PATH" >> $HOME/.bashrc
 
 pip3 install pynvim --user
-
-npm i pyright
+npm i -g pyright
 
--- remove.sh
+++ remove.sh
@@ -10,12 +10,13 @@ rm -rf $HOME/.config/nvim
 # remove plug
 rm -rf $HOME/.local/share/nvim/site/autoload/plug.vim
 
+# uninstall pyright
+npm r -g pyright
+
 # remove nodejs
 rm -rf $HOME/.bin/node-v14.17.1-linux-x64
 
 # uninstall pynvim
 pip3 uninstall pynvim
 
-# uninstall pyright
-npm r -g pyright
 
