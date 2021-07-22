040131f3ba4c423dfe29fe0a7fa741b4fce27d01
Sungkeun Kim <ksungkeun84@tamu.edu>
2021-07-22 00:34:12 -0400

    .

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+xhost +local:docker
 sudo docker run --rm -i -t \
   --privileged \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:/home/sungkeun/.Xauthority \
   -v $PWD/docker-home/:/home/sungkeun/ \
   -v $HOME:/home/sungkeun/host \
+  --hostname $(hostname) \
   danguria/dev-env
-  
